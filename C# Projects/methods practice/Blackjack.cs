2bf18be1689ccb4e27354ce1d75646e21e9493a3
wooleggs <skylahrmoore@gmail.com>
2023-06-19 15:57:24 -0700

    Blackjack
    Updated and added code to multiple classes in blackjack project

---
+++ C# Projects/methods practice/Blackjack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            deck.Shuffle(3);
+
+            foreach (Card card in deck.Cards)
+            { 
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            //Console.WriteLine(cardOne.Face + " of " + cardOne.Suit);
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
+        }
+       
+        }
+      
+        }
+        
+
+    
+
+
---
+++ C# Projects/methods practice/Deck.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods_practice
+{
+    public class Deck
+    {
+        public Deck()
+        {
+            //Basic constructor set up
+            Cards = new List<Card>();
+            List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" };
+            List<string> Facees = new List<string>()
+            { "Two", "Three", "Four", "Five", "Six", "Sevem",
+               "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"
+            };
+            foreach (string Face in Facees)
+            {
+                foreach (string Suit in Suits)
+                {
+                    Card card = new Card();
+                    card.Suit = Suit;
+                    card.Face = Face;
+                    Cards.Add(card);
+                }
+            }
+
+        }
+        public List<Card> Cards { get; set; }
+
+        public void Shuffle(int times = 1)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> Templist = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(Cards.Count);
+                    Templist.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                Cards = Templist;
+            }
+            
+
+        }
+    }
+}
